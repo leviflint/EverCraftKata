46ab160a9ddf0645db084fb7fe792449e82e19bd
leviflint <tweekex@hotmail.com>
2020-11-16 20:56:45 -0500

    Basic constructer values added and tested

--- spec/character.app.spec.js
+++ spec/character.app.spec.js
@@ -1,15 +1,41 @@
-import { Character } from "../src/js/character.js";
+
 
 describe("Can create a character", () => {
-  describe("Can set a character name", () => {
+  describe("Can set starting character values", () => {
     let underTest;
 
     beforeEach(() => {
       underTest = new Character();
     });
 
-    it("Player is prompted for a name", () => {
-      expect(underTest.characterName).toBe()
-    })
+    it("Player has a name", () => {
+      underTest.characterName = "Test"
+      expect(underTest.characterName).toBe("Test")
+    });
+    it("Player alignment should be neutral", () => {
+      underTest.characterAlignment = "Neutral"
+      expect(underTest.characterAlignment).toBe("Neutral")
+    });
+    it("Player has a starting armor value of 10", () => {
+      expect(underTest.characterArmor).toBe(10)
+    });
+    it("Player has a starting stength value of 10", () => {
+      expect(underTest.characterStrength).toBe(10)
+    });
+    it("Player has a starting dexterity value of 10", () => {
+      expect(underTest.characterDexterity).toBe(10)
+    });
+    it("Player has a starting constitution value of 10", () => {
+      expect(underTest.characterConstitution).toBe(10)
+    });
+    it("Player has a starting wisdom value of 10", () => {
+      expect(underTest.characterWisdom).toBe(10)
+    });
+    it("Player has a starting intelligence value of 10", () => {
+      expect(underTest.characterIntelligence).toBe(10)
+    });
+    it("Plauyer has a starting charisma value of 10", () => {
+      expect(underTest.characterCharisma).toBe(10)
+    });
   })
 }) 
--- src/js/character.js
+++ src/js/character.js
@@ -1,5 +1,16 @@
 class Character {
     constructor() {
         this.characterName;
+        this.characterAlignment;
+        this.characterArmor = 10;
+        this.characterHitPoints = 5;
+        this.characterStrength = 10;
+        this.characterDexterity = 10;
+        this.characterConstitution = 10;
+        this.characterWisdom = 10;
+        this.characterIntelligence = 10;
+        this.characterCharisma = 10;
+        this.characterExperience = 0;
     }
 }
+
